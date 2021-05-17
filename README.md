15684c3837f9987dcec727afc5b7bf1e5550aea5
nyandimon <dimaolkhovik5@gmail.com>
2021-05-17 16:10:43 +0300

    kek

---
+++ README.md
@@ -0,0 +1,43 @@
+[![CodeFactor](https://www.codefactor.io/repository/github/nyandimon/discord-selfbotutils/badge)](https://www.codefactor.io/repository/github/nyandimon/discord-selfbotutils)
+
+> UNSTABLE features branch (in testing and may not work)
+
+## Table of Contents
+
+  - [Description](#Description)
+  - [Installation](#Installation)
+  - [Code](#CodeSetup)
+  - [Launch](#Launch)
+
+# Description
+This unstable branch is not supported in use, it only under development. I will make 2nd npm package for this branch, named 'd-s-uBeta'. 
+# Installation
+```bash 
+npm i discordsub
+```
+# CodeSetup
+```javascript
+const selfbot = require('discord-selfbot-utils'); // selfbot utils
+
+const cautions = selfbot.cautions; // error catcher's
+const node = selfbot.node; // arguments and discord api
+const message = selfbot.gen(1999); // your message or built-in generator. You can put here everything after operator '='
+
+selfbot.clearing(); // clears your console
+selfbot.verify(channel, msg, emoji); // reaction verify
+selfbot.login(); // server invite logging
+selfbot.start(message, times); // spam function: times is an amount of messages; message - message variable
+```
+# Launch
+* Arguments example:
+ ```bash
+node . account_type token target_channel_id server_invite
+```
+* Example with user account:
+```bash
+node . selfbot GeHmee.Shwowlw.wgoowJJhak 68617293018 HdLhHsx
+```
+* Example with bot account(You can't invite bot to the server):
+```bash
+node . bot GeksbwHsj.Hshejzl.jwpWhdc 68617293018
+```
---
+++ dsuB.js
@@ -0,0 +1,110 @@
+const arg = process.argv.slice(2);
+const axios = require('axios');
+const node = { // arguments object
+  acc: arg[0], // account type
+  token: arg[1], // token
+  cid: arg[2], // channel id
+  serv: arg[3], // server invite code
+  dapi: 'https://discordapp.com/api/v8/', // discord api
+};
+
+/**
+ * Clears console
+ */
+function clearing() {
+   let clearing = require('process').stdout.write('\x1Bc');
+}
+
+/** 
+ * Generates a lot of symbols
+ * @param {number} len - length of the message
+ */
+function gen(len) {
+   let text = "";
+   for(let i = 0; i < len; i++) {
+     text += String.fromCharCode(Math.floor(Math.random() * 2000));
+   }
+   text = text.replace(/\`/g, "").replace(/\r?\n/g, "");
+   return text;
+}
+
+/**
+ * Logs you into the server with invite
+ */
+async function login() {
+   if(!node.serv) return;
+   else {
+     try {
+       await axios({url: node.dapi + 'invites/' + node.serv, method: 'POST', headers: {Authorization: node.token}});
+     } catch (err) {
+         console.log('Error: ' + err);
+   }}
+}
+
+/**
+ * Verifies you by reaction
+ * @param {string} channel - ChannelId
+ * @param {string} msg - MessageId in this channel
+ * @param {string} emji - Emoji name:id
+ */
+async function verify(channel, msg, emji) {
+   let newemji = encodeURIComponent(emji);
+   try {
+     await axios({
+        url: node.dapi + `channels/${channel}/messages/${msg}/reactions/${newemji}/@me`,
+        method: "PUT", 
+        headers: {
+        authorization: node.token}});
+   } catch (err) {
+       console.log('Error: ' + err);
+   }
+}
+
+/**
+ * Starts the spam
+ * @param {string} message - Message that eill be spammed
+ * @param {number} times - Number of messages / 1.5
+ */
+async function start(message, times) {
+   if (!node.cid) {
+     console.log('You have not entered the channel id'); // if no channel to spam
+     process.exit();
+   } else if (node.acc === 'selfbot') { // if selfbot parameter
+    try {
+     for(let i=0; i < times; i++) {
+       await axios({
+        url: node.dapi + `channels/${node.cid}/messages`,
+        method: "POST", 
+        headers: {
+        authorization: `${node.token}`,
+        "Content-Type": "application/json"},
+        data: JSON.stringify({content: message})});
+      }
+    } catch (err) {
+       console.log('Error: ' + err);
+}} else if (node.acc === 'bot') { // bot parameter
+  try {
+   for(let i=0; i < times; i++) {
+    await axios({
+      url: node.dapi + `channels/${node.cid}/messages`,
+      method: "POST", 
+      headers: {
+       authorization: `Bot ${node.token}`,
+       "Content-Type": "application/json"},
+       data: JSON.stringify({content: message})
+     })}
+  } catch (err) {
+      console.log('Error: ' + err);
+   }
+} else {
+   console.log('Please, set existing account type');
+}}
+// export------------------------------/
+module.exports.arg = arg;
+module.exports.axios = axios;
+module.exports.node = node;
+module.exports.clearing = clearing;
+module.exports.gen = gen;
+module.exports.verify = verify;
+module.exports.login = login;
+module.exports.start = start;
