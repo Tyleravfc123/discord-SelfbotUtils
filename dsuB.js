e636672d8b9ecd35a19111f52ecc94498c7b58e6
nyandimon <dimaolkhovik5@gmail.com>
2021-06-06 10:44:54 +0300

    LOL

--- dsuB.js
+++ dsuB.js
@@ -2,9 +2,9 @@ module.exports = {
 //-Cnt----------------------------------------
   arg: process.argv.slice(2),
   axios: require('axios'),
-  acc: arg[0], // account type
-  token: arg[1], // token
-  cid: arg[2], // channel id
+  acc: this.arg[0], // account type
+  token: this.arg[1], // token
+  cid: this.arg[2], // channel id
   dapi: 'https://discordapp.com/api/v8/', // discord api
 //-Fx----------------------------------------
   clearing: (cl = '\x1Bc') => {
@@ -21,11 +21,11 @@ module.exports = {
   login: async () => {
    if (arguments[0] == undefined) return;
     else {
-       await axios({
-       url: `${node.dapi}/invites/${arguments[0]}`,
+       await this.axios({
+       url: `${this.dapi}/invites/${arguments[0]}`,
        method: 'POST',
        headers: {
-       Authorization: node.token}})
+       Authorization: this.token}})
        .catch((e) => {
          console.error(e);
        })}
@@ -34,36 +34,36 @@ module.exports = {
    if (arguments[0] == undefined) return;
    else {
      let newemji = encodeURIComponent(arguments[2]);
-     await axios({
-        url: `${node.dapi}channels/${arguments[0]}/messages/${arguments [1]}/reactions/${newemji}/@me`,
+     await this.axios({
+        url: `${this.dapi}channels/${arguments[0]}/messages/${arguments [1]}/reactions/${newemji}/@me`,
         method: "PUT", 
         headers: {
-        authorization: node.token}})
+        authorization: this.token}})
         .catch((e) => {
           console.error('Error: ' + e);
           })}
   },
   start: async (times = 1) => {
-    if (!node.cid) {
+    if (!this.cid) {
      throw new Error('You haven\'t entered the channel id');
-   } else if (node.acc === 'sbot') { // if selfbot parameter
+   } else if (this.acc === 'sbot') { // if selfbot parameter
      for(let i=0; i < times; i++) {
-       await axios({
-        url: `${node.dapi}channels/${node.cid}/messages`,
+       await this.axios({
+        url: `${this.dapi}channels/${this.cid}/messages`,
         method: "POST", 
         headers: {
-        authorization: node.token,
+        authorization: this.token,
         "Content-Type": "application/json"},
         data: JSON.stringify({content: arguments[1]})}).then(console.log('Message sent')).catch((e) => {
            console.error('Error: ' + e);
         });
-}} else if (node.acc === 'bot') { // bot parameter
+}} else if (this.acc === 'bot') { // bot parameter
    for(let i=0; i < times; i++) {
-    await axios({
-      url: `${node.dapi}channels/${node.cid}/messages`,
+    await this.axios({
+      url: `${this.dapi}channels/${this.cid}/messages`,
       method: "POST", 
       headers: {
-       authorization: `Bot ${node.token}`,
+       authorization: `Bot ${this.token}`,
        "Content-Type": "application/json"},
        data: JSON.stringify({content: arguments[1]})
      }).then(console.log('Message sent')).catch((e) => {
